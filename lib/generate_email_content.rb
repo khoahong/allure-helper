2d68a9e97e054eee797fcf820c795c533abf8ec5
khoahong <hongakhoa@gmail.com>
2019-11-22 16:19:36 +0800

    Fix issue with parallel testing

--- lib/generate_email_content.rb
+++ lib/generate_email_content.rb
@@ -3,21 +3,16 @@ require 'require_all'
 require_all 'lib/allure_helper'
 
 env = ENV["ENVIRONMENT"]
-parent_build_url = ENV["PARENT_BUILD_URL"]
-
-jobs = JobParser.new(parent_build_url).parse
-
+build_url = "#{ENV["BUILD_URL"]}allure"
 suites = []
-jobs.each do |job|
-  next if job.name == "Email-Reporting-Test" || job.alias_name == "EXCLUDE_FROM_EMAIL_REPORTING"
 
-  config = AllureHelper::Configuration.new.tap do |config|
-    config.build_url = URI::encode("#{job.url}allure")
-    config.suite_name = job.name.to_s + (job.alias_name == "" ? "" : " (#{job.alias_name})").to_s
-  end
 
-  suites << AllureExtractor.extract_allure_content(config)
+config = AllureHelper::Configuration.new.tap do |config|
+  config.build_url = URI::encode(build_url)
+  config.suite_name = 'abc'
 end
+
+suites << AllureExtractor.extract_allure_content(config)
 
 html = EmailReport.new.build_email(suites, env)
 
